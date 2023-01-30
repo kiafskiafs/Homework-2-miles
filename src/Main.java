0e120f14d0a3349980019ef0180c0cc1bb85c5e3
kiafs <kiafs@mail.ru>
2023-01-30 18:12:14 +0400

    added BonusMilesServices

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,13 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-
-        Scanner in = new Scanner(System.in);
-
-        System.out.print("Введите стомость билета в рублях (без копеек): ");
-        
-        int ticketPrice = in.nextInt();
-        int bonusIndex = 20;
-        int bonusMiles = ticketPrice / bonusIndex;
-
-        System.out.println("Бонусные мили: " + bonusMiles);
-        in.close();
+        /*int cost = 5000;                          тут
+        int bonusIndex = 20;                        программа
+                                                    работает
+        int bonusMiles = cost / bonusIndex;         не ссылочно,
+        System.out.println(bonusMiles);             а напрямую */
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int bonusMiles = service.calculate(price);
+        System.out.println(bonusMiles);
     }
 }
